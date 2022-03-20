14cdac87859ef3d6df997f9a384145577bc4de9f
Sourav Mitra <sourav_mitra2009@hotmail.com>
2022-03-20 08:44:43 -0700

    calculator first time

---
+++ calculator.py
@@ -0,0 +1,11 @@
+this is a program to do simple calculation
+
+# This function adds two numbers
+
+def add(x,y):
+    return x+y
+
+a= input("Enter your first number: ")
+b= input("Enter your second number:")
+print ("The sum of 2 numbers is {}".format(add(a,b)))
+
